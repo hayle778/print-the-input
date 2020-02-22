84b7cd9d70a5206cb162535bc304c503151601fc
yourUsername <yourEmail@domain.com>
2020-02-22 15:18:59 -0500

    g

---
+++ src/Party.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Party {
+
+public static void main(String[]args) {
+
+    Scanner scanner = new Scanner( System.in );
+
+    System.out.println( "Enter pokemon 1:" );
+    String pokemon1 = scanner.nextLine();
+   // System.out.println( pokemon1 );
+
+    System.out.println( "Enter pokemon 2:" );
+    String pokemon2 = scanner.nextLine();
+  //  System.out.println( pokemon2 );
+
+   System.out.println( "Enter pokemon 3" );
+   String pokemon3 = scanner.nextLine();
+
+
+System.out.println( "------------print the name of all pokemon----------------" );
+    System.out.println( "1:" + pokemon1 );
+    System.out.println( "2:" + pokemon2 );
+    System.out.println( "3:" + pokemon3 );
+
+
+
+
+
+}
+}
